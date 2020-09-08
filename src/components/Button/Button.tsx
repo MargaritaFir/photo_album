a797b7df64066222b56c794006376ebe7a95b702
Margarita <firsova@actimind.com>
2020-09-08 10:09:19 +0300

    Added Button-component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,20 @@
+import React, {useCallback} from 'react';
+
+interface IProps {
+    onClick:(id: number) => void;
+    id: number;
+}
+
+const Button:React.FC<IProps> = ({onClick, id}) => {
+
+    const onClickCallback = useCallback(() => onClick(id), [onClick, id])
+
+    return (
+        <div className="button_container">
+            <button onClick={onClickCallback} ></button>
+        </div>
+    )
+}
+
+
+export default Button;
